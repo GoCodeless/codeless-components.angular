864009133aa9248102e861bbbb18ff95df829645
Brayden Wilmoth <brayden.wilmoth@gmail.com>
2021-07-27 22:10:39 -0400

    Daniel/dev (#14)
    * product detail
    
    * Product detail improvements
    
    * product checkout implementation
    
    * dropdown component
    
    * Grid text improvement
    
    * Web bugs
    
    * web-bugs  22 and 17
    
    * conditional arrow rendering in product detail
    
    * reviews
    
    * Update components to match dashboard
    
    * shopify integration
    
    * merging
    
    * cell varition
    
    Co-authored-by: Daniel Vasquez <andresvp2002@gmail.com>

--- appify.module.ts
+++ appify.module.ts
@@ -1,46 +1,43 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterModule } from "@angular/router";
 
 // Services
-import { CodelessComponentsService } from './services/codeless-components.service'
+import { CodelessComponentsService } from "./services/codeless-components.service";
 
 // Elements
-import { AppifyButtonComponent } from './elements/appify-button/appify-button.component';
-import { AppifyImageComponent } from './elements/appify-image/appify-image.component';
-import { AppifyLineComponent } from './elements/appify-line/appify-line.component';
-import { AppifySpacerComponent } from './elements/appify-spacer/appify-spacer.component';
-import { AppifyTextComponent } from './elements/appify-text/appify-text.component';
-import { AppifyVideoComponent } from './elements/appify-video/appify-video.component';
-import { AppifyIndicatorsComponent } from './elements/appify-indicators/appify-indicators.component';
+import { AppifyButtonComponent } from "./elements/appify-button/appify-button.component";
+import { AppifyImageComponent } from "./elements/appify-image/appify-image.component";
+import { AppifyLineComponent } from "./elements/appify-line/appify-line.component";
+import { AppifySpacerComponent } from "./elements/appify-spacer/appify-spacer.component";
+import { AppifyTextComponent } from "./elements/appify-text/appify-text.component";
+import { AppifyVideoComponent } from "./elements/appify-video/appify-video.component";
+import { AppifyIndicatorsComponent } from "./elements/appify-indicators/appify-indicators.component";
 
 // Navigation
-import { AppifyNavigationHeaderComponent } from './navigation/appify-navigation-header/appify-navigation-header.component';
-import { AppifyNavigationFooterComponent } from './navigation/appify-navigation-footer/appify-navigation-footer.component';
+import { AppifyNavigationHeaderComponent } from "./navigation/appify-navigation-header/appify-navigation-header.component";
+import { AppifyNavigationFooterComponent } from "./navigation/appify-navigation-footer/appify-navigation-footer.component";
 
 // Components
-import { AppifyCarouselComponent } from './components/appify-carousel/appify-carousel.component';
-import { AppifyCtaComponent } from './components/appify-cta/appify-cta.component';
-import { AppifyFeatureComponent } from './components/appify-feature/appify-feature.component';
-import { AppifyFormComponent } from './components/appify-form/appify-form.component';
-import { AppifyGridComponent } from './components/appify-grid/appify-grid.component';
-import { AppifyGridCellVariation000Component } from './components/appify-grid/cell-variation-000/grid-cell-variation-000.component';
-import { AppifyGridCellVariation001Component } from './components/appify-grid/cell-variation-001/grid-cell-variation-001.component';
-import { AppifyListComponent } from './components/appify-list/appify-list.component';
-import { AppifyNewsletterComponent } from './components/appify-newsletter/appify-newsletter.component';
-import { AppifySliderComponent } from './components/appify-slider/appify-slider.component';
-import { AppifyTestimonialComponent } from './components/appify-testimonial/appify-testimonial.component'
-import { AppifyWallComponent } from './components/appify-wall/appify-wall.component';
-
+import { AppifyCarouselComponent } from "./components/appify-carousel/appify-carousel.component";
+import { AppifyCtaComponent } from "./components/appify-cta/appify-cta.component";
+import { AppifyFeatureComponent } from "./components/appify-feature/appify-feature.component";
+import { AppifyFormComponent } from "./components/appify-form/appify-form.component";
+import { AppifyGridComponent } from "./components/appify-grid/appify-grid.component";
+import { AppifyGridCellVariation000Component } from "./components/appify-grid/cell-variation-000/grid-cell-variation-000.component";
+import { AppifyGridCellVariation001Component } from "./components/appify-grid/cell-variation-001/grid-cell-variation-001.component";
+import { AppifyListComponent } from "./components/appify-list/appify-list.component";
+import { AppifyNewsletterComponent } from "./components/appify-newsletter/appify-newsletter.component";
+import { AppifySliderComponent } from "./components/appify-slider/appify-slider.component";
+import { AppifyTestimonialComponent } from "./components/appify-testimonial/appify-testimonial.component";
+import { AppifyWallComponent } from "./components/appify-wall/appify-wall.component";
+import { AppifyDropdownComponent } from "./components/appify-dropdown/appify-dropdown.component";
 // Layouts
-import { AppifyHorizontalStackComponent } from './layouts/appify-horizontal-stack/appify-horizontal-stack.component';
-import { AppifyVerticalStackComponent } from './layouts/appify-vertical-stack/appify-vertical-stack.component';
+import { AppifyHorizontalStackComponent } from "./layouts/appify-horizontal-stack/appify-horizontal-stack.component";
+import { AppifyVerticalStackComponent } from "./layouts/appify-vertical-stack/appify-vertical-stack.component";
 
 @NgModule({
-    imports: [
-        CommonModule,
-        RouterModule
-    ],
+    imports: [CommonModule, RouterModule],
     declarations: [
         AppifyButtonComponent,
         AppifyImageComponent,
@@ -53,6 +50,7 @@ import { AppifyVerticalStackComponent } from './layouts/appify-vertical-stack/ap
         AppifyCtaComponent,
         AppifyFeatureComponent,
         AppifyGridComponent,
+        AppifyDropdownComponent,
         AppifyGridCellVariation000Component,
         AppifyGridCellVariation001Component,
         AppifyNavigationHeaderComponent,
@@ -70,11 +68,10 @@ import { AppifyVerticalStackComponent } from './layouts/appify-vertical-stack/ap
         AppifyWallComponent,
         AppifyIndicatorsComponent,
         AppifyHorizontalStackComponent,
-        AppifyVerticalStackComponent
+        AppifyVerticalStackComponent,
     ],
-    providers: [
-        CodelessComponentsService
-    ],
+    providers: [CodelessComponentsService],
+
     exports: [
         AppifyButtonComponent,
         AppifyImageComponent,
@@ -89,6 +86,7 @@ import { AppifyVerticalStackComponent } from './layouts/appify-vertical-stack/ap
         AppifyFeatureComponent,
         AppifyFormComponent,
         AppifyGridComponent,
+        AppifyDropdownComponent,
         AppifyIndicatorsComponent,
         AppifyListComponent,
         AppifyNewsletterComponent,
@@ -96,11 +94,8 @@ import { AppifyVerticalStackComponent } from './layouts/appify-vertical-stack/ap
         AppifyTestimonialComponent,
         AppifyWallComponent,
         AppifyHorizontalStackComponent,
-        AppifyVerticalStackComponent
+        AppifyVerticalStackComponent,
     ],
-    entryComponents: [
-
-    ]
+    entryComponents: [],
 })
-
 export class AppifyModule {}
--- elements/appify-text/appify-text.component.ts
+++ elements/appify-text/appify-text.component.ts
@@ -1,24 +1,24 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
 
-import { CodelessComponentsService } from '../../services/codeless-components.service'
-import { StyleFont, StylePadding } from '../../models/styles.model'
+import { CodelessComponentsService } from "../../services/codeless-components.service";
+import { StyleFont, StylePadding } from "../../models/styles.model";
 
-import { EditBlockElementItem } from '../appify-image/appify-image.component'
+import { EditBlockElementItem } from "../appify-image/appify-image.component";
 
 export enum TextType {
-    header = 'header',
-    body = 'body'
+    header = "header",
+    body = "body",
 }
 
 export enum TextWidth {
-    full = 'full',
-    margin = 'margin'
+    full = "full",
+    margin = "margin",
 }
 
 export enum Alignment {
-    left = 'left',
-    center = 'center',
-    right = 'right'
+    left = "left",
+    center = "center",
+    right = "right",
 }
 
 export class TextStyle {
@@ -28,41 +28,47 @@ export class TextStyle {
 }
 
 @Component({
-  selector: 'appify-text',
-  templateUrl: './appify-text.component.html',
-  styleUrls: ['./appify-text.component.css']
+    selector: "appify-text",
+    templateUrl: "./appify-text.component.html",
+    styleUrls: ["./appify-text.component.css"],
 })
 export class AppifyTextComponent implements OnInit {
-    @Input() identifier: string = ''
-    @Input() text: String = ''
-    @Input() textType: TextType = TextType.header
-    @Input() alignment: Alignment = Alignment.left
-    @Input() width: TextWidth = TextWidth.full
-    @Input() style: TextStyle = new TextStyle()
+    @Input() identifier: string = "";
+    @Input() text: String = "";
+    @Input() textType: TextType = TextType.header;
+    @Input() alignment: Alignment = Alignment.left;
+    @Input() width: TextWidth = TextWidth.full;
+    @Input() style: TextStyle = new TextStyle();
 
     @Output() editBlockElement = new EventEmitter<EditBlockElementItem>();
-    hoveringElement: string = null
-    hoveringIndex: number = 0
+    hoveringElement: string = null;
+    hoveringIndex: number = 0;
 
     /// Return the enum value computed in the component since enum is not
     /// accessible outside of this scope.
-    get textTypeValue() { return TextType; }
-    get textWidthValue() { return TextWidth; }
-    get alignmentValue() { return Alignment; }
+    get textTypeValue() {
+        return TextType;
+    }
+    get textWidthValue() {
+        return TextWidth;
+    }
+    get alignmentValue() {
+        return Alignment;
+    }
 
-    constructor(public componentsService: CodelessComponentsService) { }
-    ngOnInit() { }
+    constructor(public componentsService: CodelessComponentsService) {}
+    ngOnInit() {}
 
     cleanPixelText(string) {
-        return string.replace(/px/g,'');
+        return string.replace(/px/g, "");
     }
 
     emitBlockSelect(index, type) {
-        let item: EditBlockElementItem = new EditBlockElementItem()
-        item.identifier = this.identifier
-        item.index = index
-        item.selectedType = type
+        let item: EditBlockElementItem = new EditBlockElementItem();
+        item.identifier = this.identifier;
+        item.index = index;
+        item.selectedType = type;
 
-        this.editBlockElement.emit(item)
+        this.editBlockElement.emit(item);
     }
 }
